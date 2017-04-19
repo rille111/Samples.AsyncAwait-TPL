5356241a3ec10304ff68384e6f207a0be6392b2b
Rickard Robin <rickard.robin@adlibris.com>
2017-04-19 10:25:02 +0200

    .NET Core example of calling async code in a Sync way

---
+++ AsyncAwaitConsoleApp/src/AsyncAwaitConsoleApp/Message.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace AsyncAwaitConsoleApp
+{
+    public class Message
+    {
+        public static async Task<Message> CreateMessageAsync(string text)
+        {
+            await Task.Delay(500);
+            return new Message { Text = $"Text: {text}\n" };
+        }
+
+
+        public string Text { get; set; }
+    }
+}
---
+++ AsyncAwaitConsoleApp/src/AsyncAwaitConsoleApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CallAsyncMethodsSync();
+
+            CreateStartAndReturnTasks();
+
+            // Exit
+            Console.WriteLine("\n\nHit any key to exit ..");
+            Console.ReadKey();
+        }
+
+        private static void CreateStartAndReturnTasks()
+        {
+        }
+
+        /// <summary>
+        /// Depending on the application type - a running Task will run and block in a context (SynchronizationContext) and may run into a deadlock depending on how it is called.
+        /// The problem is that an UI Thread/Context waits for the Task to complete, and the Task waits for the UI Thread to complete = Deadlock! 
+        /// ASP.Net applications have a request context that allows only one thread at a time and the same problem may happen as for example a WindowsForms application, or a Unit Test runner.
+        /// 
+        /// Recommendations: Don't block on async code. 
+        /// 1. Use async/await all the way
+        /// 2. Wrap the async code and call it where you actually have detailed control.
+        /// 3. In your “library” async methods, use ConfigureAwait(false) wherever possible. Ugly hack.
+        /// </summary>
+        /// <remarks>
+        /// http://blog.stephencleary.com/2012/07/dont-block-on-async-code.html
+        /// </remarks>
+        private static void CallAsyncMethodsSync()
+        {
+            // Unsafe. Calling .Result blocks the main thread, and can deadlock if the caller is on the thread pool itself.
+            var task1 = Message.CreateMessageAsync("test 1");
+            Console.WriteLine(task1.Result.Text);
+
+            // Safer, but can still produce deadlocks.
+            // Does not resume on the context. Instead, .CreateMessageAsync will resume on a thread pool thread. 
+            // Avoid becuase it's just a hack and you would have to use it everywhere!
+            var message = Message.CreateMessageAsync("test 2")
+                .ConfigureAwait(false) // 
+                .GetAwaiter()
+                .GetResult();
+            Console.WriteLine(message.Text);
+
+            // Is completely safe for all application types
+            var task3 = Task.Run(async () => await Message.CreateMessageAsync("test 4"));
+            Console.WriteLine(task3.Result.Text);
+        }
+    }
+}
